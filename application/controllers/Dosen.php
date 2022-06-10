baabfa0a4f18f803587f86ecf2531d4151049f46
Faisal Akhdan <faisalakhdan50@gmail.com>
2022-06-10 18:11:49 +0700

    Menambahkan Praktikum 11&12

--- application/controllers/Dosen.php
+++ application/controllers/Dosen.php
@@ -75,5 +75,10 @@ class Dosen extends CI_Controller{
         $this->dosen->delete($data_dosen);
         redirect('dosen','refresh');
     }
+    public function upload(){
+        $_iddosen=$this->input->post("iddosen");
+        $this->load->model('dosen_model', 'dosen');
+        $dosen = $this->
+    }
 }
 ?>
